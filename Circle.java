efc9cdcf3d7003f0211294b86fcdb4cb744e39c0
nicolecheng <ncheng@stuy.edu>
2015-12-10 13:15:44 -0500

    Circle class and Area interface done

---
+++ Circle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Circle extends Shape implements Area{
+
+    private double radius;
+
+    public Circle(double radius, String name){
+	super(name);
+	this.radius = radius;
+    }
+
+    public double getRadius(){
+	return radius;
+    }
+
+    public String toString(){
+	return "Circle "+getName()+" with radius of "+getRadius();
+    }
+
+    public double getArea(){
+	return radius * radius * Math.PI;
+    }
+	
+
+}
