ddd488cae4e37f30874e5bffdb414ae5c94775c5
nicolecheng <ncheng@stuy.edu>
2015-12-11 04:01:37 -0500

    getArea() and toString() added

--- Sphere.java
+++ Sphere.java
@@ -1,10 +1,16 @@
-public class Sphere extends Circle{
+import java.util.*;
+
+public class Sphere extends Circle implements Volume{
 
     public Sphere(double radius, String name){
 	super(radius,name);
     }
+    
+    public double getArea(){
+    	return Math.PI * (4/3) * radius * radius * radius;
+    }
 
     public String toString(){
-	return "FIXME!";
+	return "Sphere "+getName()+" with radius of "+getRadius();
     }
 }
